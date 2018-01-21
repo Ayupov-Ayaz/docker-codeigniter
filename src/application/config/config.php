64ca1f6cd35dda986eab7c74b7179e94c51779b1
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-01-21 20:53:59 +0300

    логирование приложения

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -239,7 +239,7 @@ $config['log_threshold'] = 0;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = '/var/www/logs/';
 
 /*
 |--------------------------------------------------------------------------
